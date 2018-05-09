77fc693bc2d06935c8df8c97f787584dd8405137
TsiarasKon <tsiaraskon@gmail.com>
2018-05-09 22:37:46 +0300

    A: Web site names

--- webcreator.sh
+++ webcreator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Input checking:
 if [ ! -d "$1" ]; then
 	echo root_directory \""$1"\" does not exist.
 	exit 1
@@ -20,3 +21,28 @@ if [ "$lines_num" -lt 10000 ]; then
 	echo text_file must have at least 10000 lines
 	exit 1
 fi
+w1=$(($3 - 1))
+p1=$(($4 - 1))
+
+# Purging root_directory, if full:
+if test "$(ls -A "$1")"; then
+    echo Warning: directory is full, purging ...
+	ls -dA1 $PWD/$1/* | xargs rm -r 		# also deletes hidden files
+fi
+
+# Create web sites names:
+sitelist=()
+for i in $(seq 0 "$w1"); do
+	di="$1"/site"$i";
+	echo "$di";
+	randnums=($(shuf -i 100-9999 -n "$4"))
+	insitelist=""
+	for j in $(seq 0 $p1); do
+		pg="$1"/site"$i"/page"$j"_"${randnums[$j]}".html;
+		echo " $pg";
+		insitelist+="$pg ";
+	done
+	sitelist+=("$insitelist");
+done
+
+echo ${sitelist[@]}
